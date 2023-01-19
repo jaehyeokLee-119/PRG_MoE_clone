4ca3c06b91318eb8242e850aa0c4e75afd437893
Lee Jae Hyeok <hjl88190705@gmail.com>
2023-01-19 17:14:58 +0900

    feat: modules 추가

--- module/trainer.py
+++ module/trainer.py
@@ -1,5 +1,17 @@
-
+from numpy import hamming
+from .preprocessing import *
+import module.model as M
+from .evaluation import *
+import torch.optim as optim
+from torch.utils.data import TensorDataset, DataLoader
+from tqdm import tqdm
+import logging, datetime
+import sklearn
 
 class learning_env:
-    def run(self, **kwargs):
-        pass
+    def __init__(self, gpus, train_data, valid_data, test_data, split_directory, max_seq_len, log_directory, model_name, port, contain_context, data_label, **kwargs) -> None:
+        self.gpus = gpus
+        self.single_gpu = True if len(self.gpus) == 1 else False 
+        
+        self.train_dataset, self.valid_dataset, self.test_dataset = train_data, valid_data, test_data
+        
